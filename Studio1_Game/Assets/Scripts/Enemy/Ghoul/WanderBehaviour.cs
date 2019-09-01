21110c9efeb1b06387ca5b477cfd7987d7d7efe0
Madshtik <jann_cosio@yahoo.com>
2019-09-01 15:55:52 +0400

    Just changed cam pos in test. No effect on game

--- Studio1_Game/Assets/Scripts/Enemy/Ghoul/WanderBehaviour.cs
+++ Studio1_Game/Assets/Scripts/Enemy/Ghoul/WanderBehaviour.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class WanderBehaviour : Node
 {
+    Vector3 circleCentre;
     float circleRadius = 5;
     float wanderAngle = Random.Range(0f, Mathf.PI * 2);
     Vector3 SeekDirection;
     public override void MyLogicUpdate()
     {
-        
+        circleCentre = new Vector3((bTManager as GhoulBehaviourTree).transform.position.x, 
+            (bTManager as GhoulBehaviourTree).transform.position.y, (bTManager as GhoulBehaviourTree).transform.position.z + 5);
 
 
         myCurrentState = State.FAILED;
